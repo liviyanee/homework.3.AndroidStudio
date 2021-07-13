accb2786555139c0cb7359a96622d6d7c70bc3f3
liviyanee <liviya.nee@icloud.com>
2021-07-13 20:00:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework3as/splash_activity.java
@@ -0,0 +1,19 @@
+package com.example.homework3as;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class splash_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+
+    }
+
